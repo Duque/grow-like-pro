570d2ece4a79e37cf95e04290969ab6a20d58a62
Duque <raul.duque.montanez@gmail.com>
2016-01-09 00:39:11 +0100

    corrección puerto app.js + avances summary

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+//Dinahosting
+port = process.env.PORT || 12375;
+ip = process.env.IP || '0.0.0.0';
+
+
+
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

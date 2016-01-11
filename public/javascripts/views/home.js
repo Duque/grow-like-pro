1694dd59e2516e276830a954a5d0213ded255a4c
Duque <raul.duque.montanez@gmail.com>
2016-01-11 08:43:03 +0100

    maqueta de sumario finalizada

--- public/javascripts/views/home.js
+++ public/javascripts/views/home.js
@@ -10,5 +10,14 @@ Home = Backbone.View.extend({
 		$('#content').html(this.template());
 		return this;
 	},
+
 })
 
+//Eventos
+var homeEvents = {};
+HomeEvents=_.extend(homeEvents, Backbone.Events);
+
+//See growing details
+homeEvents.bind('verCultivo', function(el){
+	alert('Vamos a ver el cultivo '+el.attr('id'));
+});
